e1eb47791dc414cbdfb3d16d4c202f5cdb570430
Ryan-Afzal <ryan.afzal@outlook.com>
2024-02-15 14:48:45 -0500

    Create wavefnplotter.py

---
+++ wavefnplotter.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.ioff()
+fig = plt.figure(figsize=(10, 20), layout='constrained')
+plots = fig.subplot_mosaic([["psiPlot1", "psiPlot1"],
+                            ["psiPlot1", "psiPlot1"]])
+
+plots["psiPlot1"].set_title("Placeholder Title")
+plots["psiPlot1"].set_xlabel("Position (*units*)")
+plots["psiPlot1"].set_ylabel("Y-axis title")
+
+def plot(plotId, psi, V, x0, xf, dx):
+	xArray = np.arange(x0, xf, dx)
+	nsteps = len(xArray)
+	VArray = np.zeros(nsteps)
+	psiArray = np.zeros(nsteps)
+
+	for i in range(nsteps):
+		VArray[i] = V(xArray[i])
+		psiArray[i] = psi(xArray[i])
+	
+
+	pass
