099fc2bbaae1369679ab851db8526c3596bb341f
Ryan-Afzal <ryan.afzal@outlook.com>
2024-02-19 15:57:05 -0500

    Added wavefnplotter test

--- wavefnplotter.py
+++ wavefnplotter.py
@@ -9,6 +9,10 @@ plots = fig.subplot_mosaic([["psiPlot1", "psiPlot1"],
 plots["psiPlot1"].set_title("Placeholder Title")
 plots["psiPlot1"].set_xlabel("Position (*units*)")
 plots["psiPlot1"].set_ylabel("Y-axis title")
+plots["psiPlot1"].set_ylim(0, 10)
+
+def plot(psi, V, x0, xf, dx):
+	plot("psiPlot1", psi, V, x0, xf, dx)
 
 def plot(plotId, psi, V, x0, xf, dx):
 	xArray = np.arange(x0, xf, dx)
@@ -20,6 +24,12 @@ def plot(plotId, psi, V, x0, xf, dx):
 		VArray[i] = V(xArray[i])
 		psiArray[i] = psi(xArray[i])
 	
-	
+	plots[plotId].plot(xArray, VArray, label="Potential V(x)")
+	plots[plotId].plot(xArray, psiArray, label="Wavefunction Ψ(x)")
+	plots[plotId].legend()
 
-	pass
+
+
+
+
+#ψ(x)
