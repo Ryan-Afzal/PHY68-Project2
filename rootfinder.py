2388704cbf7d759a81d31ff3cf3bcd2eb2b2d4bf
JWazzle <76964355+JWazzle@users.noreply.github.com>
2024-02-20 19:08:15 -0500

    tweak

--- rootfinder.py
+++ rootfinder.py
@@ -1,11 +1,11 @@
 import scipy.optimize as op
 import math
 
-def f(x):
-    return math.sin(x)
+# def f(x):
+#     return math.sin(x)
 
-x0 = 6
-# def rootFind(f, x0):
-op.fsolve(f, x0)
+# x0 = 6
+def rootFind(f, x0):
+    op.fsolve(f, x0)
 
-# rootFind(f, x0)
+# rootFind(f, x0):
